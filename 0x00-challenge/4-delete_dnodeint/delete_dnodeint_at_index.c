d11204c549110383a2eddf0a10251a53c65c83fe
thenicopixie <391@holbertonschool.com>
2019-01-18 06:54:34 +0000

    delete node

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,10 +43,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-                (*head)->prev->next = (*head)->next
+                (*head)->prev->next = (*head)->next;
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
-                free(*head)
+                free(*head);
 		*head = saved_head;
 	}
 	return (1);
